899e75692f8b62cc3f79948530bf78901d1e2851
Wafiul Achdi <achdiwafiul@gmail.com>
2024-11-07 23:36:58 +0700

    add product sorting

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,10 +7,44 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::orderBy('created_at', 'DESC')->paginate(12);
-        return view('shop', compact('products'));
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $order_column = "";
+        $order_order = "";
+        $order = $request->query('order') ? $request->query('order') : -1;
+        switch($order)
+        {
+            case 1:
+                $order_column = "created_at";
+                $order_order = "DESC";
+                break;
+            case 2:
+                $order_column = "created_at";
+                $order_order = "ASC";
+                break;
+            case 3:
+                $order_column = "sale_price";
+                $order_order = "ASC";
+                break;
+            case 4:
+                $order_column = "sale_price";
+                $order_order = "DESC";
+                break;
+            case 5:
+                $order_column = "name";
+                $order_order = "ASC";
+                break;
+            case 6:
+                $order_column = "name";
+                $order_order = "DESC";
+                break;
+            default:
+                $order_column = "id";
+                $order_order = "DESC";
+        }
+        $products = Product::orderBy($order_column, $order_order)->paginate($size);
+        return view('shop', compact('products', 'size', 'order'));
     }
 
     public function product_details($product_slug)
